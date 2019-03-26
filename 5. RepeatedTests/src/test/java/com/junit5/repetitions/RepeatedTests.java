a675aeefd0feebe15319a06524c9dcf96f5bf542
Vladimir101 <vladimir100@hotmail.com>
2019-03-26 11:32:20 -0700

    RepeatedTests

--- 5. RepeatedTests/src/test/java/com/junit5/repetitions/RepeatedTests.java
+++ 5. RepeatedTests/src/test/java/com/junit5/repetitions/RepeatedTests.java
@@ -2,15 +2,28 @@ package com.junit5.repetitions;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 class RepeatedTests
 {
-	@DisplayName("repeat test")
-	@RepeatedTest(value = 2, name = "{displayName}: repetition {currentRepetition} of {totalRepetitions}")
-	void repeatTest(TestInfo testInfo)
+	@DisplayName("test with the default displayName")
+	@RepeatedTest(value = 2, name = "{displayName}")//: repetition {currentRepetition} of {totalRepetitions}")
+	void repeatedTest(TestInfo testInfo)
 	{
-		System.out.println("This is the name of the test: " + testInfo.getDisplayName());
+		System.out.println("repeatedTest name: " + testInfo.getDisplayName());
+	}
+	
+	@DisplayName("test with the long displayName")
+	@RepeatedTest(value = 2, name = RepeatedTest.LONG_DISPLAY_NAME)
+	void repeatedTestWithLongName(TestInfo testInfo)
+	{
+		System.out.println("repeatedTestWithLongName name: " + testInfo.getDisplayName());
+	}
+	
+	@DisplayName("test with the short displayName")
+	@RepeatedTest(value = 2, name = RepeatedTest.SHORT_DISPLAY_NAME)
+	void repeatedTestWithShortName(TestInfo testInfo)
+	{
+		System.out.println("repeatedTestWithShortName name: " + testInfo.getDisplayName());
 	}
 }
