5df3cccf8713f5900a2609a1e368a8395f4796c2
Vladimir101 <vladimir100@hotmail.com>
2020-01-16 12:31:29 -0800

    Parallel tests

---
+++ 9. ParallelTests/src/test/java/Test1.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@Execution(ExecutionMode.CONCURRENT)
+class Test1
+{
+	@Test
+	void test1() throws InterruptedException
+	{
+		System.out.println("1");
+		Thread.sleep(10000);
+	}
+	
+	@Test
+	void test2() throws InterruptedException
+	{
+		System.out.println("2");
+		Thread.sleep(10000);
+	}
+}
