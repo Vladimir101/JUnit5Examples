ce0a7a66a1011c20da6279b236f740c56d4f8728
Vladimir101 <vladimir100@hotmail.com>
2020-07-17 18:53:31 -0700

    added comments

--- 9. ParallelTests/src/test/java/ParallelTests.java
+++ 9. ParallelTests/src/test/java/ParallelTests.java
@@ -1,9 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
+/*There are multiple ways to set these configuration values, 
+ * one is to use the Maven Surefire plugin for it:
+
+<plugin>
+  <artifactId>maven-surefire-plugin</artifactId>
+  <version>2.22.2</version>
+  <configuration>
+    <properties>
+      <configurationParameters>
+        junit.jupiter.execution.parallel.enabled = true
+        junit.jupiter.execution.parallel.mode.default = concurrent
+      </configurationParameters>
+    </properties>
+  </configuration>
+</plugin>
+
+* or a junit-platform.properties file inside src/test/resources 
+* with the configuration values as content.*/
 @Execution(ExecutionMode.CONCURRENT)
 class ParallelTests
 {
