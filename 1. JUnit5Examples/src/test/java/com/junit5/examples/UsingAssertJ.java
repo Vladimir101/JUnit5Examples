6f77292252889552277a684c08528d2c25d290e7
Vladimir101 <vladimirbelor100@gmail.com>
2020-11-06 12:03:58 -0800

    added AssertJ examples

---
+++ 1. JUnit5Examples/src/test/java/com/junit5/examples/UsingAssertJ.java
@@ -0,0 +1,35 @@
+package com.junit5.examples;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+class UsingAssertJ
+{
+	private List<String> list = Arrays.asList("1", "2", "3");
+	private Map<String, Integer> map = Map.of("One", 1, "Two", 2, "Three", 3);
+	
+	@Test
+	void listTest()
+	{
+		assertThat(list)
+		  .isNotEmpty()
+		  .contains("1")
+		  .containsSequence("2", "3")
+		  .doesNotContain("13");
+	}
+	
+	@Test
+	void mapTest()
+	{
+		assertThat(map)
+		  .isNotEmpty()
+		  .containsKey("One")
+		  .doesNotContainKeys("Ten")
+		  .contains(entry("Three", 3));
+	}
+}
