0f84c53dcbbf741713fdfdce2c7b9bed7a987c4a
Vladimir101 <vladimirbelor100@gmail.com>
2020-12-13 13:57:37 -0800

    Allure reports

---
+++ 11. AllureReports/src/test/java/reporting/MyTest.java
@@ -0,0 +1,14 @@
+package reporting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyTest
+{
+	@Test
+	void test()
+	{
+		assertTrue(true);
+	}
+}
