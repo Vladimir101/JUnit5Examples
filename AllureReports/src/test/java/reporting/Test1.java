d8f4486b4116171c9a4f3eb66068deecf2d43aca
Vladimir101 <vladimirbelor100@gmail.com>
2020-12-13 13:41:28 -0800

    Allure reports example

---
+++ AllureReports/src/test/java/reporting/Test1.java
@@ -0,0 +1,16 @@
+package reporting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1
+{
+	@Test
+	void test()
+	{
+		boolean a = true;
+		assertThat(a).isTrue();
+	}
+}
