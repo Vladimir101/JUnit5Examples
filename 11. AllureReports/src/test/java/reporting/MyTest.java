b2cb5b5966c62dd2406ed7a6091e92b2d87f1e89
Vladimir101 <vladimirbelor100@gmail.com>
2020-12-13 18:52:51 -0800

    added Allure annotations

--- 11. AllureReports/src/test/java/reporting/MyTest.java
+++ 11. AllureReports/src/test/java/reporting/MyTest.java
@@ -4,9 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Epic("Login Tests Epic")
+@Feature("Invalid Login Features")
 class MyTest
 {
 	@Test
+	@Story("User tries to login the system with empty username and invalid password.")
+	@Description("This is my test to verify Allure reports")
 	void test()
 	{
 		assertTrue(true);
--- 11. AllureReports/src/test/java/reporting/Test1.java
+++ 11. AllureReports/src/test/java/reporting/Test1.java
@@ -1,13 +1,18 @@
 package reporting;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+
+@Epic("Login Tests Epic")
 class Test1
 {
 	@Test
+	@Story("This is a Fail Story.")
+    @Description("This is a Fail Story Description.")
 	void test()
 	{
 		boolean a = true;
